5d39e9f3963eec6fac613e208ba00268f7f108a7
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-12-19 13:34:30 -0500

    Made lots of changes, forgot to commit. Added new div to help seperate content better when viewing specific drink. Adjusted script to load data correctly for each drink, and added opinion buttons. Adjusted styling for new elements as well.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function fetchDrinks(){
 function loadDrinks(drinks){
     let drinkMenu = document.getElementById('drink-menu');
     let sideMenu = document.createElement('table');
+    let drinkDisplay = document.createElement('div');
+    let displayMenu = document.getElementById('display-menu');
 
     drinks.forEach(drink=> {
         let drinkCard = document.createElement('div');
@@ -30,18 +32,14 @@ function loadDrinks(drinks){
         drinkCard.appendChild(drinkName);
         drinkMenu.appendChild(drinkCard);
 
-        let displayName = document.createElement('h1');
-        displayName.innerHTML = drink.name;
-        displayName.setAttribute('class', 'display-names');
-
-        let displayPic = document.createElement('img');
-        displayPic.src = drink.image;
-        displayPic.setAttribute('class', 'display-imgs');
-
         sideMenu.setAttribute('id', 'side-menu');
         let sideMenuItem = document.createElement('tr');
         sideMenuItem.innerHTML = drink.name;
         sideMenu.appendChild(sideMenuItem);
+        sideMenuItem.addEventListener('click', ()=>{
+            drinkDisplay.textContent ='';
+            createDrinkDisplay(drinkDisplay, displayMenu, drink);
+        })
 
         drinkCard.addEventListener('mouseenter', ()=>{
             drinkName.removeAttribute('hidden');
@@ -53,7 +51,8 @@ function loadDrinks(drinks){
         })
         drinkCard.addEventListener('click', ()=>{
             drinkMenu.textContent="";
-            createSideMenu(sideMenu, drinkMenu);
+            createSideMenu(sideMenu, displayMenu);
+            createDrinkDisplay(drinkDisplay, displayMenu, drink);
         })
 
     });
@@ -90,10 +89,60 @@ function createDrinks(){
     })
 }
 
-function createSideMenu(sideMenu, drinkMenu){
+function createSideMenu(sideMenu, displayMenu){
     let menuHeader = document.createElement('tr');
     menuHeader.innerHTML = "Drink Menu"
     menuHeader.setAttribute('id', 'menu-header');
     sideMenu.prepend(menuHeader);
-    drinkMenu.append(sideMenu);
+    displayMenu.append(sideMenu);
+}
+
+function createDrinkDisplay(drinkDisplay, displayMenu, drink){
+    let displayName = document.createElement('h1');
+    displayName.innerHTML = drink.name;
+    displayName.setAttribute('class', 'display-names');
+
+    let displayPic = document.createElement('img');
+    displayPic.src = drink.image;
+    displayPic.setAttribute('class', 'display-imgs');
+
+    drinkDisplay.append(displayName, displayPic);
+    drinkDisplay.setAttribute('id', 'drink-display');
+    displayMenu.append(drinkDisplay);
+
+    let ingredientsHeader = document.createElement('h3');
+    ingredientsHeader.innerHTML = 'Ingredients'
+    drinkDisplay.append(ingredientsHeader);
+
+    let ingredients = Object.values(drink.recipe.ingredients);
+    ingredients.forEach(ingredient => {
+        let ingredientItem = document.createElement('li');
+        ingredientItem.innerHTML = ingredient;
+        drinkDisplay.append(ingredientItem);
+    })
+
+    let instructionsHeader = document.createElement('h3');
+    instructionsHeader.innerHTML = 'Instructions'
+    drinkDisplay.append(instructionsHeader);
+
+    let instructionList = document.createElement('ol');
+    drinkDisplay.append(instructionList);
+
+    let instructions = Object.values(drink.recipe.instructions);
+    instructions.forEach(step => {
+        let stepItem = document.createElement('li');
+        stepItem.innerHTML = step;
+        instructionList.append(stepItem);
+    })
+
+    let likeButton = document.createElement('button');
+    likeButton.innerHTML = 'Favorite';
+    likeButton.setAttribute('class', 'opinion-button');
+
+    let dislikeButton = document.createElement('button');
+    dislikeButton.innerHTML = 'Not a Fan';
+    dislikeButton.setAttribute('class', 'opinion-button');
+
+    drinkDisplay.append(likeButton, dislikeButton);
+
 }
